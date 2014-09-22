071e990408b9e8393356b45ee0b01da6dd7095e5
Atsushi Nakajyo <jyotti@gmail.com>
2014-09-22 12:48:48 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'elasticsearch'
+maintainer       'Atsushi Nakajyo'
+maintainer_email 'jyotti@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures elasticsearch'
+long_description 'Installs/Configures elasticsearch'
+version          '0.1.0'
+
